cf4ec482a5688caea7ca6eebb50d6e940838d5cf
yashsh13 <yashpat1302@gmail.com>
2025-11-27 17:25:04 +0530

    Added user and admin authentication

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,16 +1,49 @@
 import { Router } from "express";
+import { adminModel } from "../db.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 const adminRouter = Router();
 
-adminRouter.post('/signup',(req,res)=>{
+adminRouter.post('/signup', async (req,res)=>{
+
+    const { email, password, firstname, lastname} = req.body;
+
+    //add zod, hashing
+
+    await adminModel.create({
+        email:email,
+        password:password,
+        firstname:firstname,
+        lastname:lastname
+    })
+
     return res.json({
         msg:"You have signedup as admin"
     })
 })
 
-adminRouter.post('/login',(req,res)=>{
+adminRouter.post('/login',async (req,res)=>{
+
+    const { email, password} = req.body;
+
+    const admin = await adminModel.findOne({
+        email:email,
+        password:password
+    })
+
+    if(!admin){
+        return res.status(403).json({
+            msg:"Invalid Credentials for admin"
+        })
+    }
+
+    const token = await jwt.sign({id:admin._id},process.env.JWT_ADMIN_PASS);
+    //store token in browser 
+
     return res.json({
-        msg:"You have logged in as admin"
+        msg:"You have logged in as admin",
+        token:token
     })
 })
 
--- Routes/user.js
+++ Routes/user.js
@@ -1,17 +1,52 @@
 import { Router } from "express";
 import { userModel } from "../db.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 const userRouter = Router();
 
-userRouter.post('/signup',(req,res)=>{
+userRouter.post('/signup',async (req,res)=>{
+
+    const { email, password, firstname, lastname } = req.body;
+
+    //Add zod, hash password etc
+
+    await userModel.create({
+        email:email,
+        password:password,
+        firstname:firstname,
+        lastname:lastname
+    })
+
     return res.json({
         msg:"You have signed up as user"
     })
 })
 
-userRouter.post('/login',(req,res)=>{
+userRouter.post('/login',async (req,res)=>{
+
+    const { email, password } = req.body;
+
+    //zod, unhash password etc
+
+    const user = await userModel.findOne({
+        email:email,
+        password:password
+    })
+
+    if(!user){
+        res.status(403).json({
+            msg:"Invalid Credentials"
+        })
+    }
+
+    const token = await jwt.sign({id:user._id},process.env.JWT_USER_PASS);
+
+    //store the token in browser
+
     return res.json({
-        msg:"You have logged in as user"
+        msg:"You have logged in as user",
+        token:token
     })
 })
 
